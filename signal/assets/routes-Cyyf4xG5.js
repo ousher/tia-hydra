70caae89bb17b3f7fbd5b4d4da847aff192428fa
Ondrej Sramek <ondrej@tia-framework.com>
2026-09-18 08:34:00 +0200

    Cyber Signal v2 — statická aplikace v /signal/, bez účtu a bez serveru

---
+++ signal/assets/routes-Cyyf4xG5.js
@@ -0,0 +1,1 @@
+import{f as e,t}from"./digests-CdURH-Ni.js";import{t as n}from"./cut-view-DxRv8Fz6.js";var r=e();function i(){return(0,r.jsx)(n,{date:t})}export{i as component};
--- signal/assets/routes-D4xo0JbY.js
+++
@@ -1,1 +0,0 @@
-import{f as e,t}from"./digests-Du_VT8r6.js";import{t as n}from"./cut-view-BPxoz1HP.js";var r=e();function i(){return(0,r.jsx)(n,{date:t})}export{i as component};
